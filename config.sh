d185ad75f76a17b1d825d1f722db7570bd2cfa63
redlag27 <48453242+redlag27@users.noreply.github.com>
2019-03-19 23:32:23 +0100

    Add files via upload

---
+++ config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get install python2.7
+
+sudo apt-get install python-pip
+
+pip install --upgrade google-api-python-client
+
+pip install --upgrade oauth2client
+
+pip install inotifyx
